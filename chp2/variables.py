65539ce8995d243988623e49b4aa958b92a30490
Ibrahim Sattar <bsse23004@itu.edu.pk>
2025-09-27 20:13:07 +0500

    Add initial implementation of variable assignments and print statements

---
+++ chp2/variables.py
@@ -0,0 +1,27 @@
+a=9
+b=True
+c= 6.9
+d= "gya ee?"
+e= 'n'
+f= None
+g= str(c)
+h = float(a)
+i = int(c)
+j = bool(f)
+k= chr(a)
+
+print(type(a),type(b),type(c),type(d),type(e),type(f),type(g),type(h),type(i),type(j),type(k))
+print(a,b,c,d,e,f,g,h,i,j,k)
+a-=-1
+print(a)
+
+if (a==10 and c ==6.9):
+    print(a+c)
+
+print(10%7,"avg=",(a+c)/2,"a^c=",a**i)
+
+a = int(input("Enter +ive number to loop\t"))
+
+for i in range(a):
+    print("gya "+"ee?")
+
