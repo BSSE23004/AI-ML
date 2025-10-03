1670a2e649995c8fb9b68240bf27cf67d2cfd23b
Ibrahim Sattar <bsse23004@itu.edu.pk>
2025-10-03 11:59:17 +0500

    Add detailed properties of sets and examples for set operations, including unique number collection

--- chp5/dictionary&Sets.py
+++ chp5/dictionary&Sets.py
@@ -72,7 +72,8 @@ print("------------------Sets------------------")
 print('''Properties of Sets:
 1. A set is a collection which is unordered, unchangeable*, and unindexed.
 2. Sets are written with curly brackets.
-3. Set items are unordered, unchangeable, and do not allow duplicate values.''')
+3. Set items are unordered, unchangeable, and do not allow duplicate values.
+4. Sets cannot contain mutable items like lists, dictionaries, or other sets, but they can contain immutable items like strings, numbers, or tuples.''')
 set1 = {}
 print(type(set1))#dict
 set2 = {1, 2, 3}
@@ -107,43 +108,48 @@ print("A.union(B)",A.union(B))#{'microsoft', 'banana', 'cherry', 'google', 'appl
 print(" A.intersection(B)",A.intersection(B))#{'apple'}
 print("A.symmetric_difference(B)",A.symmetric_difference(B))#{'microsoft', 'banana', 'cherry', 'google'}
 print("A.difference(B)",A.difference(B))#{'banana', 'cherry'}
-print(B.difference(A))#{'microsoft', 'google'}
-print(A.isdisjoint(B))#False
+print("B.difference(A)",B.difference(A))#{'microsoft', 'google'}
+print("A.isdisjoint(B)",A.isdisjoint(B))#False
 C = {"kiwi", "mango"}
+print("C",C)#{'kiwi', 'mango'}
 print("A.isdisjoint(C)",A.isdisjoint(C))#True
 print("A.issubset(B)",A.issubset(B))#False
 D = {"apple", "banana"}
+print("D",D)#{'banana', 'apple'}
 print("D.issubset(A)",D.issubset(A))#True
 print("A.issuperset(D)",A.issuperset(D))#True
 E = {"apple", "kiwi"}
+print("E",E)#{'kiwi', 'apple'}
 print("A.issuperset(E)",A.issuperset(E))#False
 F = {"banana", "cherry", "apple", "kiwi", "mango"}
+print("F",F)#{'banana', 'cherry', 'kiwi', 'mango', 'apple'}
 print("A.issubset(F)",A.issubset(F))#True
 print("F.issuperset(A)",F.issuperset(A))#True
 G = {"banana", "cherry", "apple", "kiwi", "mango"}
+print("G",G)#{'banana', 'cherry', 'kiwi', 'mango', 'apple'}
 A.update(G)
 print("After Updating A with G")
-print(A)#{'banana', 'cherry', 'kiwi', 'mango',
-print(B)#{'microsoft', 'google', 'apple'}
+print("A",A)#{'banana', 'cherry', 'kiwi', 'mango',
+print("B",B)#{'microsoft', 'google', 'apple'}
 B.update(A)
 print("After Updating B with A")
-print(B)#{'microsoft', 'banana', 'kiwi', 'cherry',
-print(A)#{'banana', 'cherry', 'kiwi', 'mango',
+print("B",B)#{'microsoft', 'banana', 'kiwi', 'cherry',
+print("A",A)#{'banana', 'cherry', 'kiwi', 'mango',
 A.intersection_update(B)
 print("After Intersection Update of A with B")
-print(A)#{'banana', 'cherry', 'apple'}
-print(B)#{'microsoft', 'banana', 'kiwi', 'cherry',
+print("A",A)#{'banana', 'cherry', 'apple'}
+print("B",B)#{'microsoft', 'banana', 'kiwi', 'cherry',
 A.symmetric_difference_update(B)
 print("After Symmetric Difference Update of A with B")
-print(A)#{'microsoft', 'kiwi', 'apple', 'mango',
-print(B)#{'microsoft', 'banana', 'kiwi', 'cherry',
+print("A",A)#{'microsoft', 'kiwi', 'apple', 'mango',
+print("B",B)#{'microsoft', 'banana', 'kiwi', 'cherry',
 A.difference_update(B)
 print("After Difference Update of A with B")
-print(A)#{'apple', 'mango'}
-print(B)#{'microsoft', 'banana', 'kiwi', 'cherry',
+print("A",A)#{'apple', 'mango'}
+print("B",B)#{'microsoft', 'banana', 'kiwi', 'cherry',
 A = {"apple", "banana", "cherry"}
 B = {"google", "microsoft", "apple"}
-print(A,B)#{'banana', 'cherry', 'apple'} {'microsoft', 'google', 'apple'}
+print("A,B",A,B)#{'banana', 'cherry', 'apple'} {'microsoft', 'google', 'apple'}
 print("A|B",A | B)#{'microsoft', 'banana', 'cherry', 'google', 'apple'}
 print("A&B",A & B)#{'apple'}
 print("A^B",A ^ B)#{'microsoft', 'banana', 'cherry', 'google'}
@@ -155,3 +161,21 @@ print("D<=A",D <= A)#True
 print("A>=D",A >= D)#True
 E = {"apple", "kiwi"}
 print("E<=A",E <= A)#False
+
+practiceSet = {69, "Sixty Nine", "69",69.69,69.0}
+print(practiceSet)#{69, 'Sixty Nine', '69', 69.69}
+print(len(practiceSet))#4
+print(type(practiceSet))#set
+practiceSet.clear()
+# practiceSet = {[1,2,3], (4,5,6), {7,8,9}}#TypeError: unhashable type: 'list'
+
+
+uniqueNumbers = set()
+
+for i in range(1, 11):
+    num = int(input("Enter a number: "))
+    uniqueNumbers.add(num)
+
+print("Unique Numbers: ", uniqueNumbers)
+print("Count of Unique Numbers: ", len(uniqueNumbers))
+
